d90b2c88fa70d7904628daea3dd835e47fc98fa2
boffoli <boffoli@gmail.com>
2024-04-16 11:11:14 +0200

    bug fix

--- obj/Debug/net8.0/BoffToolkit.AssemblyInfo.cs
+++ obj/Debug/net8.0/BoffToolkit.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Toolkit supporting base activities: caching, pooling, configuration\r\n            " +
     "        exceptions, type loading")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.9.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.9+f6817a2939010fa9206850751e467f16316aa60c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.9+de84a32a44da3233cdb826b82cea5dc62c3bb521")]
 [assembly: System.Reflection.AssemblyProductAttribute("BoffToolkit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BoffToolkit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.9.0")]
